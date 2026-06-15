9f67734741ce2202e7d203c0f6c2ba4c6f87f76d
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-06-15 11:39:52 +0200

    feat: initialize simulation engine and render dashboard

--- ElevatorSimulator.ConsoleUI/Program.cs
+++ ElevatorSimulator.ConsoleUI/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+using ElevatorSimulator.ConsoleUI.Views;
+using ElevatorSimulator.Core.Entities;
+using ElevatorSimulator.Infrastructure.Services;
+using ElevatorSimulator.Simulation;
+using ElevatorSimulator.Simulation.Strategies;
+
+var building = new Building(floorCount: 10, elevatorCount: 2, elevatorCapacity: 8, tickDurationMs: 500);
+
+var eventBus = new InMemoryEventBus();
+var clock = new SimulationClock(TimeSpan.FromMilliseconds(building.TickDurationMs));
+var engine = new SimulationEngine(clock, eventBus);
+
+var strategy = new NearestElevatorStrategy();
+var dispatcher = new Dispatcher(strategy, eventBus);
+
+// Set up UI
+var dashboard = new DashboardView();
+
+// Start simulation async
+_ = Task.Run(() => engine.Play());
+
+// Start UI rendering loop blocking the main thread
+dashboard.RenderLoop(engine, clock, building);
