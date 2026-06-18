4cbc171d316b17f4ef0ac866f80fa9fb3328c6c2
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-06-18 16:55:50 +0200

    feature: prompt user for building config

---
+++ ElevatorSimulator.ConsoleUI/Configuration/ConfigurationModel.cs
@@ -0,0 +1,10 @@
+namespace ElevatorSimulator.ConsoleUI.Configuration;
+
+public class SimulationConfiguration
+{
+    public int FloorCount { get; set; }
+    public int BasementFloors { get; set; }
+    public int ElevatorCount { get; set; }
+    public int ElevatorCapacity { get; set; }
+    public int TickDurationMs { get; set; }
+}
